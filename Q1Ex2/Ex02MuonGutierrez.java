6246de93c8f87b9caee2558128704de054911f94
Ally Gutierrez <90668408+allygtrrz@users.noreply.github.com>
2022-09-20 11:56:45 +0800

    Update Ex02MuonGutierrez.java

--- Q1Ex2/Ex02MuonGutierrez.java
+++ Q1Ex2/Ex02MuonGutierrez.java
@@ -16,31 +16,55 @@ public class Ex02MuonGutierrez {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int guesses = 3;
+        String choice, again;
         
-        System.out.println("Welcome to Higher or Lower! What will you do?\n" +
+        do {
+            System.out.println("Welcome to Higher or Lower! What will you do?\n" +
                 "-Start game\n" +
                 "-Change settings\n" +
-                "-End application");
-        String response = sc.nextLine();
-        switch(response) {
-            case "Start game":
-                int random = (int) Math.floor(Math.random()*10) + 1;
-                for (int remaining = guesses; remaining > 0; remaining--) {
-                    System.out.print("You have " + remaining + " guess(es) left. What is your guess? ");
-                    int answer = Integer.parseInt(sc.nextLine());
-                    if (answer < random) {
-                        System.out.print("Guess higher! ");
-                    } else if (answer > random) {
-                        System.out.print("Guess lower! ");
-                    } else if (answer == random) {
-                        System.out.print("You got it!");
-                        break;
-                    } else {
-                        System.out.print("You lost...");
-                        break;
-                    }
-                }
-                break;
-        }
+                "-End application\n");
+            choice = sc.nextLine();
+        
+            switch(choice) {
+                case ("Start game"):
+                    int random = (int) Math.floor(Math.random()*10) + 1;
+                    
+                    do {
+                        System.out.print("You have " + guesses + " guess(es) left. What is your guess? ");
+                        int guess = Integer.parseInt(sc.nextLine());
+                        guesses--;
+                        
+                        if (guesses == 0) {
+                            System.out.print("You lost...\n");
+                            break;
+                        }
+                        else {
+                            if (guess == random) {
+                                System.out.print("You got it!\n");
+                                break;
+                            }
+                            else if (guess < random) {
+                                System.out.print("Guess higher! ");
+                            }
+                            else if (guess > random) {
+                                System.out.print("Guess lower! ");
+                            }
+                        }
+                    } while (guesses != 0);
+                    
+                    break;
+                case ("Change settings"):
+                    System.out.print("How many guesses are allowed? ");
+                    guesses = Integer.parseInt(sc.nextLine());
+                    break;
+                case ("End application"):
+                    System.out.println("Thank you for playing!");
+                    System.exit(0);
+                    break;
+            }
+            
+            System.out.print("\nRun again? (y/n) ");
+            again=sc.nextLine();
+        } while (again.equalsIgnoreCase("y"));
     }
 }
