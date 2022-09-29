ea41d1c5e146d90616019bf355b4f99320eaf48b
Ally Gutierrez <90668408+allygtrrz@users.noreply.github.com>
2022-09-29 11:26:53 +0800

    Add files via upload

---
+++ Q1Ex3/Driver.java
@@ -0,0 +1,17 @@
+package ex03muongutierrez;
+
+public class Driver {
+    public static void main(String[] args) {
+        int driverNumber;
+        String fullName; // first and last name
+        String team;
+        String nationality;
+        
+        public Driver (int d, String f, String t, String n) {
+            driverNumber = d;
+            fullName = f;
+            team = t;
+            nationality = n;
+        }
+    }
+}
