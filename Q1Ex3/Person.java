881dfb75596f96baabae58a69cdb912bff94a153
Ally Gutierrez <90668408+allygtrrz@users.noreply.github.com>
2022-11-02 21:37:26 +0800

    Update and rename Fan.java to Person.java

--- Q1Ex3/Person.java
+++ Q1Ex3/Person.java
@@ -1,9 +1,9 @@
-public class Fan {
+public class Person {
     String name;
     Song favoriteSong;
     Singer favoriteSinger;
 
-    public Fan(String name, Song favoriteSong, Singer favoriteSinger){
+    public Person(String name, Song favoriteSong, Singer favoriteSinger){
         this.name = name;
         this.favoriteSong = favoriteSong;
         this.favoriteSinger = favoriteSinger;
