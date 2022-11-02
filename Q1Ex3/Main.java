d99687add0f3f038196ac239d8fd36046bb953c4
Ally Gutierrez <90668408+allygtrrz@users.noreply.github.com>
2022-11-02 21:51:29 +0800

    Update Main.java

--- Q1Ex3/Main.java
+++ Q1Ex3/Main.java
@@ -12,14 +12,14 @@ public class Main {
         Singer a3 = new Singer("Arctic Monkeys", s3);
         Singer a4 = new Singer("NIKI", s4);
 
-        Person f1 = new Person("Ally", s1, a1);   // me
-        Person f2 = new Person("Claire", s2, a2); // my friend
-        Person f3 = new Person("Vicky", s3, a3);  // my friend
-        Person f4 = new Person("Julia", s4, a4);  // my sister
+        Person p1 = new Person("Ally", s1, a1);   // me
+        Person p2 = new Person("Claire", s2, a2); // my friend
+        Person p3 = new Person("Vicky", s3, a3);  // my friend
+        Person p4 = new Person("Julia", s4, a4);  // my sister
 
         a1.performForAudience(12);
-        System.out.println(a1.name + " has performed " + a1.noOfPerformances + " time(s).");
-        System.out.println(a1.name + " has earned " + a1.earnings + " euros.");
+        System.out.println(a1.name + " has performed a total of " + a1.noOfPerformances + " time(s).");
+        System.out.println(a1.name + " has earned a total of " + a1.earnings + " dollars/euros.");
 
         System.out.println(a1.name + "'s favorite song is " + a1.favoriteSong.title + ".");
         a1.changeFavSong(s6);
