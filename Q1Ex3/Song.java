dda698eccdd9186dff5c466ab0b6780a58c2f641
Ally Gutierrez <90668408+allygtrrz@users.noreply.github.com>
2022-11-02 22:08:21 +0800

    Delete Q1Ex3 directory

--- Q1Ex3/Song.java
+++
@@ -1,9 +0,0 @@
-public class Song {
-    String title, album, artist;
-
-    public Song(String title, String album, String artist){
-        this.title = title;
-        this.album = album;
-        this.artist = artist;
-    }
-}
